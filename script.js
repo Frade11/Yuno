d9506df0beaae1b09b9dd26c776cfcd60266f431
Frade11 <gurdisartemii@gmail.com>
2025-07-04 02:06:39 +0300

    v1.0.1
    работает генерация постов через масив но почасто вручную,адаптация под телефоны

--- script.js
+++ script.js
@@ -17,3 +17,47 @@ window.addEventListener("DOMContentLoaded", () => {
 function exit(){
     window.location.replace("auth.html");
 }
+
+const posts={
+  1: {
+      id: 1,
+      icon: "./images/recomend-icon-2.jpg",
+      nickname: "Furina Fan",
+      image: "./images/post-1.jpg",
+      likes: 456,
+      description: "LOOK AT THIS PHOTO,ITS AMAZING!!!"
+  },
+  2: {
+      id: 2,
+      icon: "./images/recomend-icon-3.jpg",
+      nickname: "Furu kuru",
+      image: "./images/post-2.jpg",
+      likes: 34059,
+      description: "See you tomorow Stella <3"
+  }
+}
+
+function DisplayPosts(mainSectionId,items){
+    const section = document.getElementById(mainSectionId);
+    if(!section) return;
+
+    section.innerHTML = items.map(post =>`
+        <div class="post-data">
+            <div class="author">
+                <img src="${post.icon}" alt="user_icon">
+                <span>${post.nickname}</span>
+            </div>
+            <div class="post-image">
+                <img src="${post.image}">
+            </div>
+            <div class="post-description">
+                <span class="material-symbols-outlined">favorite</span>
+                <h4>${post.likes}</h4>
+                <h3>${post.description}</h3>
+            </div>
+        </div>
+      `).join('');
+}
+ document.addEventListener("DOMContentLoaded", () => {
+  DisplayPosts("mainSectionId", Object.values(posts));
+});
